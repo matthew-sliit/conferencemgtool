5d0208b5cce5309cfe578f269129df1554c14776
amandaaaim <amandamahawatta99@gmail.com>
2021-06-29 23:22:58 +0530

    visitor updated

--- client/visitor/App.jsx
+++ client/visitor/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from "./Component/home";
+import Keynotes from "./Component/keynotes";
 export default () => {
     return <Router>
         <Switch>
             <Route exact path={"/"}>
-                <p>Home page</p>
+                <Home/>
+            </Route>
+            <Route exact path={"/keynotes"}>
+                <Keynotes/>
             </Route>
         </Switch>
     </Router>
